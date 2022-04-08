5e1ce4bc83a71830e9a1da229d1b36a4ce122fb6
Sid <71106886+sidr-champlain@users.noreply.github.com>
2022-04-07 22:40:17 -0400

    Delete gitpush.sh

--- gitpush.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /c/Users/pilfu/Documents/Final-Project-SYS265
-git add .
-git config user.name sidr-champlain
-git config user.email sidr.champlain@protonmail.com
-git commit -m "Updated Repo"
-git push
-echo "Complete"
